8c609b8ad6a75b727f7668611679bff1309ad460
Chris Price <christopher.r.price@gmail.com>
2020-02-11 00:43:42 -0500

    Accounted for case, punctuation, and spacing. Chained a lot of methods together though. Might look like crap.

---
+++ Palindrome Checker Playground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+//func isPalindrome(word: String) -> Bool {
+//    let reversed = String(word.reversed())
+//    if word.lowercased() == reversed.lowercased() {
+//            return true
+//    } else {
+//        return false
+//    }
+//}
+//
+//isPalindrome(word: "taCocat")
+
+/* Accounting for case, punctuation, and spacing. */
+
+func palindrome(word: String) -> Bool {
+    let string = word.components(separatedBy: CharacterSet.punctuationCharacters).joined().replacingOccurrences(of: " ", with: "").lowercased()
+    if string == String(string.reversed()) {return true} else {return false}
+}
+
+palindrome(word: "A man; a plan; a canal; Panama!")
+
+
