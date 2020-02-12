8a037766b5acf72af494afbfc0aeb6092dca6b61
Chris Price <christopher.r.price@gmail.com>
2020-02-11 21:38:45 -0500

    reorganized to see if I could make the code clearer.

--- Palindrome Checker Playground.playground/Contents.swift
+++ Palindrome Checker Playground.playground/Contents.swift
@@ -13,11 +13,18 @@ import Foundation
 
 /* Accounting for case, punctuation, and spacing. */
 
-func palindrome(word: String) -> Bool {
-    let string = word.components(separatedBy: CharacterSet.punctuationCharacters).joined().replacingOccurrences(of: " ", with: "").lowercased()
-    if string == String(string.reversed()) {return true} else {return false}
-}
+//func palindrome(word: String) -> Bool {
+//    let string = word.components(separatedBy: CharacterSet.punctuationCharacters).joined().replacingOccurrences(of: " ", with: "").lowercased()
+//    if string == String(string.reversed()) {return true} else {return false}
+//}
 
+func palindrome(word: String) -> Bool {
+    let depunctuated = word.components(separatedBy: CharacterSet.punctuationCharacters).joined()
+    let despaced = depunctuated.replacingOccurrences(of: " ", with: "")
+    let leveled = despaced.lowercased()
+    
+    if leveled == String(leveled.reversed()) {return true} else {return false}
+}
 palindrome(word: "A man; a plan; a canal; Panama!")
 
 
